fc6385296f28a0a231af973697243981f0d8c413
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-03-01 22:33:43 +0300

    Last Modif pour deploiement BDD

--- src/main/java/com/vente/voiture/mongo/ClientConnection.java
+++ src/main/java/com/vente/voiture/mongo/ClientConnection.java
@@ -6,6 +6,6 @@ import com.mongodb.client.MongoClients;
 public class ClientConnection{
 
     public static MongoClient GetMongoClient(){
-        return MongoClients.create("mongodb://mongo:H6CAA5c2ahhCb2DbFEFBa2gAHbDCeHgF@monorail.proxy.rlwy.net:40448");
+        return MongoClients.create("mongodb://mongo:b11bbcb1cBa3AEC45cce2aCbD-e1dcAf@viaduct.proxy.rlwy.net:35157");
     }
 }
